3016df17a0bcadb4a33915391ed2f5ac58991a71
M0n4d <m0n4d1000@gmail.com>
2018-12-15 17:52:54 +1000

    foundation removed, material-ui added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import {TopBar, Menu, Button} from 'react-foundation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TopBar>
-          <Menu>
-            <Button color={"success"}>Test1</Button>
-            <Button>Test2</Button>
-            <Button>Test3</Button>
-          </Menu>
-        </TopBar>
+
     </div>
     );
   }
