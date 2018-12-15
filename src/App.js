7c4f082f764df8949557b4513e9845caa160a956
Diego <dandida955@hotmail.com>
2018-12-15 03:06:01 -0600

    Auto stash before merge of "dan" and "origin/monad"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createMuiTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0091EA",
+      light: "#D81B60"
+    },
+    secondary: {
+      main: "#00BCD4",
+      light: "#E53935"
+    }
+  }  
+});
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        </Button>
-        Hello World
-        <Button variant="contained" color="primary">
-      </div>
-    );
-  }
+function SimpleAppBar(props) {
+
+  return (
+    <div className="TopBar">
+      <AppBar position="static" theme="primary">
+        <Toolbar>
+          <Typography variant="h6" color="secondary">
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default App;
+SimpleAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default SimpleAppBar;
