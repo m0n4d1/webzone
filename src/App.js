34f8635d455bb2bd9215a6d7fe198b30fbe68955
Diego <dandida955@hotmail.com>
2018-12-25 12:39:40 -0600

    deleted css styling that was not used

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { createMuiTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { Header } from 'semantic-ui-react';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#0091EA",
-      light: "#D81B60"
-    },
-    secondary: {
-      main: "#00BCD4",
-      light: "#E53935"
-    }
-  }  
-});
+const HomeHeader = () => (
+    <div>
 
-const theme = createMuiTheme({palette: {
-    primary: {
-      main: "#0091EA",
-      light: "#D81B60"
-    },
-    secondary: {
-      main: "#D32F2F",
-      light: "#0091EA"
-    }
-  }  
-}, "R");
-
-
-function SimpleAppBar(props) {
-
-  return (
-    <div className="TopBar" theme="R">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" color="secondary-main">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
+        <Header as='h1'> Home page </Header>
+    
     </div>
-  );
-}
+)
 
-SimpleAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default SimpleAppBar;
+export default HomeHeader;
