8836adbff58d1be4cdb1c28ea1deeb8bc5604932
Diego <dandida955@hotmail.com>
2018-12-25 20:10:07 -0600

    played around with semantic ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import H from './header';
 import {Menu} from 'semantic-ui-react';
 
-const items = [
-    { key: 'Home', active: true, name: 'Home' },
-    { key: 'About us', name: 'About us' },
-    { key: 'Suggestions', name: 'Suggestions' },
-  ]
 
-const menu = () => <Menu items={items} />;
+const a = [{key: 'navBar', active: true, name: 'NavBar'}, {key: 'home', name:'Home'}, {key: 'event', name:'Events'}, { key: 'about', name:'About us' },]
+const M = () => <Menu items={a} />
 
 
- export default menu;
+class App extends Component {
+    
+    render() {
+        return (<div className="root">
+                    <H/>
+                    <M/>
+                </div>);
+    }
+
+}
+
+ export default App;
---
+++ src/header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+const H = () => (<Header as="h1"> some text </Header>);
+
+export default H;
