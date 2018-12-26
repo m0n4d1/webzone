544c11e372851a7fac7d3e36ad40051bdcb40f73
Diego <dandida955@hotmail.com>
2018-12-25 20:49:15 -0600

    played with semantic ui

--- src/header.js
+++ src/header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Header} from 'semantic-ui-react';
 
-const H = () => (<Header as="h1"> some text </Header>);
+const H = () => (
+<div className="ui header centered">
+    <Header as="h1"> some text </Header>
+</div>);
 
 export default H;
