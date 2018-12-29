a6a59453f698470ef6241852ceacf9401ade1e44
Monad <fern@localhost.localdomain>
2018-12-29 16:11:54 +1000

    semantic ui nav setup

--- src/navigation/Header.js
+++ src/navigation/Header.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
 
 export default class Header extends Component {
   render() {
     return (
-      <nav>
-        <h2>This is the Header</h2>
-        <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/blog">Blog</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-        </ul>
-      </nav>
+        // <Menu stackable>
+        //   <h2>This is the Header</h2>
+        //   <ul>
+        //     <li><NavLink >Home</NavLink></li>
+        //     <li><NavLink to="/blog">Blog</NavLink></li>
+        //     <li><NavLink to="/about">About</NavLink></li>
+        //   </ul>
+        <Menu stackable>
+          <Menu.Item>
+            <img src='https://via.placeholder.com/50' />
+          </Menu.Item>
+
+          <Menu.Item
+            as={NavLink}
+            exact
+            to="/"
+            name='Home'
+
+          >
+            Home
+          </Menu.Item>
+
+          <Menu.Item
+            as={NavLink}
+            to="/blog"
+            name='Blog'
+          >
+            Blog
+          </Menu.Item>
+
+          <Menu.Item
+            as={NavLink}
+            to="/about"
+            name='sign-in' >
+            About
+          </Menu.Item>
+        </Menu>
     )
   }
 }
