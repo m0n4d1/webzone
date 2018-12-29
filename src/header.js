0c209862f9a1217d81406ecfaeb66e2e09b99e7d
Monad <fern@localhost.localdomain>
2018-12-29 14:39:53 +1000

    clean slate

--- src/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Header} from 'semantic-ui-react';
-
-const H = () => (
-<div className="ui header centered">
-    <Header as="h1"> some text </Header>
-</div>);
-
-export default H;
