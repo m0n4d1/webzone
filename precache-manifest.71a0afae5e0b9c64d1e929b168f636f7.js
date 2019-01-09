585a7ce97502596dcab332184376f0ecc315e03c
M0n4d <m0n4d1000@gmail.com>
2019-01-10 01:54:01 +1000

    Updates

---
+++ precache-manifest.71a0afae5e0b9c64d1e929b168f636f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5a2a60bae16d73d5613",
+    "url": "/webzone/static/js/runtime~main.f5a2a60b.js"
+  },
+  {
+    "revision": "d316cb6fe98916ef17e5",
+    "url": "/webzone/static/js/main.d316cb6f.chunk.js"
+  },
+  {
+    "revision": "2274efd7bad8cfa3d52f",
+    "url": "/webzone/static/js/1.2274efd7.chunk.js"
+  },
+  {
+    "revision": "bb335b1db5f82212158234c45b3594cb",
+    "url": "/webzone/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/webzone/precache-manifest.d50c2cd253c4dfbfddc4b87d2b66106e.js"
+  "/webzone/precache-manifest.71a0afae5e0b9c64d1e929b168f636f7.js"
 );
 
 workbox.clientsClaim();
